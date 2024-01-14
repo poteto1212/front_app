52385879cc0cd70ec5a52ce17a9a66c2c61a5730
Makoto Ito <poteto1212@ASUSVivobook>
2024-01-14 22:57:23 +0900

    サーバーからの呼び出し処理の実装

--- src/src/components/forms/predict_dm_1_form.tsx
+++ src/src/components/forms/predict_dm_1_form.tsx
@@ -1,164 +1,211 @@
 import styled from "styled-components";
-import {memo,useState} from 'react'
+import { memo, useState } from 'react'
 import axios from 'axios';
 
 const Predict_dm_1_form = () => {
-    // 身長　体重　空腹時血糖　3時間血糖下面積　血漿インスリン下面積　定常状態血糖値
-    const [ height , setHeight ] = useState<number>()
-    const [ bw , setBw ] = useState<number>()
-    const [ fbs , setFbs ] = useState<number>()
-    const [ glc_3_auc , setGlc_3_auc ] = useState<number>()
-    const [ ins_plasma_auc , setIns_plasma_auc ] = useState<number>()
-    const [ glc_css , setGlc_css ] = useState<number>()
+  // 身長　体重　空腹時血糖　3時間血糖下面積　血漿インスリン下面積　定常状態血糖値
+  const [height, setHeight] = useState<number>()
+  const [bw, setBw] = useState<number>()
+  const [fbs, setFbs] = useState<number>()
+  const [glc_3_auc, setGlc_3_auc] = useState<number>()
+  const [ins_plasma_auc, setIns_plasma_auc] = useState<number>()
+  const [glc_css, setGlc_css] = useState<number>()
 
-    // 入力体重の取得
-    const input_Height = (height: number) => {
-      setHeight(height)
+  //予測結果
+  //API呼び出し処理を作成する
+  const [data, setData] = useState(
+    {
+      height: null,
+      bw: null,
+      fbs: null,
+      glc_3_auc: null,
+      glc_plasma_auc: null,//TODO ここ名前おかしいのでサーバー含め修正
+      glc_css: null,
     }
+  );
 
-    const input_Bw = (bw: number) => {
-      setBw(bw)
+  const [response, setResponse] = useState<{
+    predict_val: string;
+    result: string;
+  } | null>(null);
+
+  const handle_change = (fieldName: string, value: string | number) => {
+    setData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  // 入力体重の取得
+  const input_Height = (height: number) => {
+    setHeight(height)
+    handle_change("height", height)
+  }
+
+  const input_Bw = (bw: number) => {
+    setBw(bw)
+    handle_change("bw", bw)
+  }
+
+  const input_Fbs = (fbs: number) => {
+    setFbs(fbs)
+    handle_change("fbs", fbs)
+  }
+
+  const input_Glc_3_auc = (glc_3_auc: number) => {
+    setGlc_3_auc(glc_3_auc)
+    handle_change("glc_3_auc", glc_3_auc)
+  }
+
+  const input_Ins_plasma_auc = (ins_plasma_auc: number) => {
+    setIns_plasma_auc(ins_plasma_auc)
+    handle_change("glc_plasma_auc", ins_plasma_auc)
+  }
+
+  const input_Glc_css = (glc_css: number) => {
+    setGlc_css(glc_css)
+    handle_change("glc_css", glc_css)
+  }
+
+  //responsが帰ってくるまで保留する
+  const postData = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:9004/predict_dm_1/',
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        },
+      );
+      console.log('Response:', response.data)
+      setResponse(response.data)
+    } catch (error) {//存在しないrequest等
+      console.error('Error:', error);
+      console.log(data)
     }
+  };
 
-    const input_Fbs = (fbs: number) => {
-      setFbs(fbs)
-    }
+  return (
+    <Body>
+      <Form_title>
+        <p>主な説明変数</p>
+      </Form_title>
 
-    const input_Glc_3_auc = (glc_3_auc: number) => {
-      setGlc_3_auc(glc_3_auc)
-    }
+      <Height>
+        <p>身長(cm)</p>
+      </Height>
+      <Height_input
+        type="number"
+        placeholder="身長入力"
+        name="height"
+        defaultValue=""
+        required={false}
+        value={height}
+        onChange={(e) => input_Height(e.target.valueAsNumber)}
+      />
 
-    const input_Ins_plasma_auc = (ins_plasma_auc: number) => {
-      setIns_plasma_auc(ins_plasma_auc)
-    }
+      <Bw>
+        <p>体重(kg)</p>
+      </Bw>
+      <BwInput
+        type="number"
+        placeholder="体重入力"
+        name="bw"
+        required={false}
+        value={bw}
+        onChange={(e) => input_Bw(e.target.valueAsNumber)}
+      />
 
-    const input_Glc_css = (glc_css: number) => {
-      setGlc_css(glc_css)
-    }  
+      <Fbs>
+        <p>空腹時血糖(mg/dL)</p>
+      </Fbs>
+      <Fbs_input
+        type="number"
+        placeholder="基準値: 70~110mg/dL 140以上でDM"
+        name="fbs"
+        required={false}
+        value={fbs}
+        onChange={(e) => input_Fbs(e.target.valueAsNumber)}
+      />
 
-    //予測結果
-    //API呼び出し処理を作成する
+      <Glc_3_auc>
+        <p>3時間血糖下面積</p>
+      </Glc_3_auc>
+      <Glc_3_auc_input
+        type="number"
+        placeholder="3時間血糖下面積"
+        name="glc_3_auc"
+        required={false}
+        value={glc_3_auc}
+        onChange={(e) => input_Glc_3_auc(e.target.valueAsNumber)}
+      />
 
-    //型定義
-    //axious関数
-    const url = ""
-    const post_value = {
+      <Ins_plasma_auc>
+        <p>血漿インスリン下面積</p>
+      </Ins_plasma_auc>
+      <Ins_plasma_auc_input
+        type="number"
+        placeholder="平均: 186"
+        name="ins_plasma_auc"
+        required={false}
+        value={ins_plasma_auc}
+        onChange={(e) => input_Ins_plasma_auc(e.target.valueAsNumber)}
+      />
 
-    };
-    
-    return(
-      <Body>
-        <Form_title>
-          <p>主な説明変数</p>
-        </Form_title>
+      <Glc_css>
+        <p>定常状態血糖値</p>
+      </Glc_css>
+      <Glc_cssInput
+        type="number"
+        placeholder="定常状態血糖値"
+        name="glc_css"
+        required={false}
+        value={glc_css}
+        onChange={(e) => input_Glc_css(e.target.valueAsNumber)}
+      />
 
-        <Height>
-          <p>身長(cm)</p>
-        </Height>
-        <Height_input
-          type= "number"
-          placeholder="身長入力"
-          name="height"
-          defaultValue=""
-          required={false}
-          value={height}
-          onChange = {(e)=>input_Height(e.target.valueAsNumber)}
-        />
-        
-        <Bw>
-          <p>体重(kg)</p>
-        </Bw>
-        <BwInput
-          type= "number"
-          placeholder="体重入力"
-          name="bw"
-          required={false}
-          value={bw}
-          onChange = {(e)=>input_Bw(e.target.valueAsNumber)}
-        />
+      <Input_value>
+        <Input_value_title>
+          <p>現在の入力値</p>
+        </Input_value_title>
+        <Input_value_table>
+          <Table border={1}>
+            <Thead>
+              <Tr>
+                <Th>身長</Th>
+                <Th>体重</Th>
+                <Th>空腹時血糖</Th>
+                <Th>3時間血糖下面積</Th>
+                <Th>血漿インスリン下面積</Th>
+                <Th>定常状態血糖値</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>{height}cm</Td>
+                <Td>{bw}kg</Td>
+                <Td>{fbs}mg/dL</Td>
+                <Td>{glc_3_auc}</Td>
+                <Td>{ins_plasma_auc}</Td>
+                <Td>{glc_css}mg/dL</Td>
+              </Tr>
+            </Tbody>
+          </Table>
 
-        <Fbs>
-          <p>空腹時血糖(mg/dL)</p>
-        </Fbs>
-        <Fbs_input
-          type="number"
-          placeholder="基準値: 70~110mg/dL 140以上でDM"
-          name="fbs"
-          required={false}
-          value={fbs}
-          onChange = {(e)=>input_Fbs(e.target.valueAsNumber)}
-        />
+          <p>
+            予測結果: {response && (
+              response.result
+            )}
+          </p>
 
-        <Glc_3_auc>
-          <p>3時間血糖下面積</p>
-        </Glc_3_auc>
-        <Glc_3_auc_input
-          type="number"
-          placeholder="3時間血糖下面積"
-          name="glc_3_auc"
-          required={false}
-          value={glc_3_auc}
-          onChange = {(e)=>input_Glc_3_auc(e.target.valueAsNumber)}
-        />
+        </Input_value_table>
+      </Input_value>
+      <Button onClick={postData}>予測の実行</Button>
 
-        <Ins_plasma_auc>
-          <p>血漿インスリン下面積</p>
-        </Ins_plasma_auc>
-        <Ins_plasma_auc_input
-          type="number"
-          placeholder="平均: 186"
-          name="ins_plasma_auc"
-          required={false}
-          value={ins_plasma_auc}
-          onChange = {(e)=>input_Ins_plasma_auc(e.target.valueAsNumber)}
-        />
-
-        <Glc_css>
-          <p>定常状態血糖値</p>
-        </Glc_css>
-        <Glc_cssInput
-          type="number"
-          placeholder="定常状態血糖値"
-          name="glc_css"
-          required={false}
-          value={glc_css}
-          onChange = {(e)=>input_Glc_css(e.target.valueAsNumber)}
-        />
-
-        <Input_value>
-          <Input_value_title>
-            <p>現在の入力値</p>
-          </Input_value_title>
-          <Input_value_table>
-            <Table border={1}>
-              <Thead>
-                <Tr>
-                  <Th>身長</Th>
-                  <Th>体重</Th>
-                  <Th>空腹時血糖</Th>
-                  <Th>3時間血糖下面積</Th>
-                  <Th>血漿インスリン下面積</Th>
-                  <Th>定常状態血糖値</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>{height}cm</Td>
-                  <Td>{bw}kg</Td>
-                  <Td>{fbs}mg/dL</Td>
-                  <Td>{glc_3_auc}</Td>
-                  <Td>{ins_plasma_auc}</Td>
-                  <Td>{glc_css}mg/dL</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-            <p>予測結果: </p>
-          </Input_value_table>
-        </Input_value>
-        <Button>予測の実行</Button>
-
-      </Body>
-    )
+    </Body>
+  )
 }
 const Body = styled.div`
   display: flex;
